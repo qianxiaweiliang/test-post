9a09d9bf477ec64136651cd64837aed9d144d1eb
qianxiaweiliang <zelanwang@126.com>
2016-06-06 13:08:02 +0800

    wang

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,17 +1,16 @@
 	function collect_same_elements(collection_a, object_b) {
 		//在此处写代码
-		var arr = new Array();
+		var arr = [];
 		for (var i = 0; i < collection_a.length; i++) {
-			console.log(collection_a.length + "2222");
-			var oneA = collection_a[i];
-			console.log(oneA);
+			var Array1 = collection_a[i];
+			console.log(Array1);
 
 			for (var j = 0; j < object_b.value.length; j++) {
-				var oneB = object_b.value[j];
-				console.log(oneB);
+				var Array2 = object_b.value[j];
+				console.log(Array2);
 
-				if (oneA.key == oneB) {
-					arr.push(oneA.key);
+				if (Array1.key == Array2) {
+					arr.push(Array1.key);
 					console.log(arr);
 				}
 
